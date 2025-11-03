8022226905e6bb0dadd12671bcbf543cba9b298b
Nicolas Barbosa <ngranato11@gmail.com>
2025-11-03 16:28:05 -0300

    Criando entidades

---
+++ CRUD_donos/entidades.py
@@ -0,0 +1,11 @@
+class Tutor:
+
+    def __init__(self,nome,dataDeNascimento,endereco,cidade,estado):
+        self.nome = nome
+        self.dataDeNascimento = dataDeNascimento
+        self.endereco = endereco
+        self.cidade = cidade
+        self.estado = estado
+        return
+    
+    
