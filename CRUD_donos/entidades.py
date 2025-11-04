9f9021416d2520669484882ea4d1f71dfab0fda6
Nicolas Barbosa <ngranato11@gmail.com>
2025-11-04 00:24:23 -0300

    adicionando variavel idade para o objeto Tutor

--- CRUD_donos/entidades.py
+++ CRUD_donos/entidades.py
@@ -1,11 +1,14 @@
+from datetime import datetime, date
+formatacaoParaData = "%d/%m/%Y"
+dataDoSistema = date.today()
+
 class Tutor:
 
     def __init__(self,nome,dataDeNascimento,endereco,cidade,estado):
         self.nome = nome
-        self.dataDeNascimento = dataDeNascimento
+        self.dataDeNascimento = (datetime.strptime(dataDeNascimento, formatacaoParaData)).date()
         self.endereco = endereco
         self.cidade = cidade
         self.estado = estado
+        self.idade = (dataDoSistema - self.dataDeNascimento).days//365
         return
-    
-    
