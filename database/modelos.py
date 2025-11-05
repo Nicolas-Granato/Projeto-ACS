75aeea4dd73caaa6cdbd2298613d25d2979684cf
Nicolas Barbosa <ngranato11@gmail.com>
2025-11-05 16:49:48 -0300

    troquei o nome do arquivo e das variaveis de relacionamento

--- database/modelos.py
+++ database/modelos.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, Integer, Date, Text, ForeignKey, Enum, Float
+from sqlalchemy import Column, ForeignKey, Enum, String, Integer, Date, Text, Float
 from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
@@ -12,7 +12,7 @@ class Tutor(Base):
     cidade = Column("CidadeTutor", String(255))
     telefone = Column("Telefone", String(25))
 
-    relacaoPaciente = relationship("Paciente", back_populates="relacaoTutor")
+    pacientes = relationship("Paciente", back_populates="tutor")
 
 class Veterinario(Base):
     __tablename__ = "tabela_veterinario"
@@ -20,7 +20,7 @@ class Veterinario(Base):
     id = Column("idVeterinario", Integer, primary_key=True, autoincrement=True)
     nomeVeterinario = Column("NomeVeterinario", String(255), nullable=False)
 
-    relacaoConsulta = relationship("Consulta", back_populates="relacaoVeterinario")
+    consultas = relationship("Consulta", back_populates="veterinario")
 
 class Especie(Base):
     __tablename__ = "tabela_especie"
@@ -28,8 +28,8 @@ class Especie(Base):
     id = Column("idEspecie", Integer, primary_key=True, autoincrement=True)
     nomeEspecie = Column("NomeEspecie", String(255), nullable=False)
 
-    relacaoRaca = relationship("Raca", back_populates="relacaoEspecie")
- 
+    racas = relationship("Raca", back_populates="especie")
+    
 class Raca(Base):
     __tablename__ = "tabela_raca"
 
@@ -37,8 +37,8 @@ class Raca(Base):
     nomeRaca = Column("NomeRaca", String(255), nullable=False)
     especie_id = Column("Especie", Integer, ForeignKey("tabela_especie.idEspecie"), nullable=False)
 
-    relacaoEspecie = relationship("Especie", back_populates="relacaoRaca")
-    relacaoPaciente_4 = relationship("Paciente", back_populates="relacaoRaca_2")
+    especie = relationship("Especie", back_populates="racas")
+    pacientes = relationship("Paciente", back_populates="raca")
 
 class Paciente(Base):
     __tablename__ = "tabela_paciente"
@@ -51,10 +51,10 @@ class Paciente(Base):
     raca_id = Column("Raca", Integer, ForeignKey("tabela_raca.idRaca"))
     tutor_id = Column("Tutor", Integer, ForeignKey("tabela_tutor.idTutor"))
 
-    relacaoTutor = relationship("Tutor", back_populates="relacaoPaciente")
-    relacaoConsulta_2 = relationship("Consulta", back_populates="relacaoPaciente_2")
-    relacaoRegistroDeCondicaoDoPaciente = relationship("RegistroDeCondicaoDoPaciente", back_populates="relacaoPaciente_3")
-    relacaoRaca_2 = relationship("Raca", back_populates="relacaoPaciente_4")
+    tutor = relationship("Tutor", back_populates="pacientes")
+    consultas = relationship("Consulta", back_populates="paciente")
+    registros_condicao = relationship("RegistroDeCondicaoDoPaciente", back_populates="paciente")
+    raca = relationship("Raca", back_populates="pacientes")
 
 class RegistroDeCondicoes(Base):
     __tablename__ = "tabela_de_registros_de_condicoes"
@@ -63,7 +63,7 @@ class RegistroDeCondicoes(Base):
     nomeCondicao = Column("NomeCondicao", String(255), nullable=False)
     descricao = Column("Descricao", Text)
 
-    relacaoRegistroDeCondicaoDoPaciente_2 = relationship("RegistroDeCondicaoDoPaciente", back_populates="relacaoCondicao")
+    registros_paciente = relationship("RegistroDeCondicaoDoPaciente", back_populates="condicao")
 
 class Consulta(Base):
     __tablename__ = "tabela_consultas"
@@ -74,8 +74,8 @@ class Consulta(Base):
     data = Column("Data", Date, nullable=False)
     observacoes = Column("Observacoes", Text)
 
-    relacaoVeterinario = relationship("Veterinario", back_populates="relacaoConsulta")
-    relacaoPaciente_2 = relationship("Paciente", back_populates="relacaoConsulta_2")
+    veterinario = relationship("Veterinario", back_populates="consultas")
+    paciente = relationship("Paciente", back_populates="consultas")
 
 class RegistroDeCondicaoDoPaciente(Base):
     __tablename__ = "tabela_de_registro_de_condicao_do_paciente"
@@ -85,5 +85,5 @@ class RegistroDeCondicaoDoPaciente(Base):
     condicao_id = Column("idCondicao", Integer, ForeignKey("tabela_de_registros_de_condicoes.idCondicao"))
     observacoes = Column("Observacoes", Text)
 
-    relacaoPaciente_3 = relationship("Paciente", back_populates="relacaoRegistroDeCondicaoDoPaciente")
-    relacaoCondicao = relationship("RegistroDeCondicoes", back_populates="relacaoRegistroDeCondicaoDoPaciente_2")
+    paciente = relationship("Paciente", back_populates="registros_condicao")
+    condicao = relationship("RegistroDeCondicoes", back_populates="registros_paciente")
