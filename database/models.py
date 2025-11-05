61150ccc1d5a840595fcf073962f8cc0069ea949
Nicolas Barbosa <ngranato11@gmail.com>
2025-11-05 16:43:00 -0300

    relacoes feitas, vou trocar os nomes das variaveis para tornar mais facil as querys

--- database/models.py
+++ database/models.py
@@ -1,67 +1,89 @@
 from sqlalchemy import create_engine, Column, String, Integer, Date, Text, ForeignKey, Enum, Float
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
 class Tutor(Base):
-    __tablename__ = "tutor"
+    __tablename__ = "tabela_tutor"
 
     id = Column("idTutor", Integer, primary_key=True , autoincrement=True)
-    NomeTutor = Column("NomeTutor", String(255), nullable=False)
-    Endereco = Column("EnderecoTutor", String(255))
-    Cidade = Column("CidadeTutor", String(255))
-    Telefone = Column("Telefone", String(25))
+    nomeTutor = Column("NomeTutor", String(255), nullable=False)
+    endereco = Column("EnderecoTutor", String(255))
+    cidade = Column("CidadeTutor", String(255))
+    telefone = Column("Telefone", String(25))
+
+    relacaoPaciente = relationship("Paciente", back_populates="relacaoTutor")
 
 class Veterinario(Base):
-    __tablename__ = "veterinario"
+    __tablename__ = "tabela_veterinario"
 
     id = Column("idVeterinario", Integer, primary_key=True, autoincrement=True)
-    NomeVeterinario = Column("NomeVeterinario", String(255), nullable=False)
+    nomeVeterinario = Column("NomeVeterinario", String(255), nullable=False)
+
+    relacaoConsulta = relationship("Consulta", back_populates="relacaoVeterinario")
 
 class Especie(Base):
-    __tablename__ = "especie"
+    __tablename__ = "tabela_especie"
 
     id = Column("idEspecie", Integer, primary_key=True, autoincrement=True)
-    NomeEspecie = Column("NomeEspecie", String(255), nullable=False)    
+    nomeEspecie = Column("NomeEspecie", String(255), nullable=False)
+
+    relacaoRaca = relationship("Raca", back_populates="relacaoEspecie")
  
 class Raca(Base):
-    __tablename__ = "raca"
+    __tablename__ = "tabela_raca"
 
     id = Column("idRaca", Integer, primary_key=True, autoincrement=True)
-    NomeRaca = Column("NomeRaca", String(255), nullable=False)
-    Especie = Column("Especie", Integer, ForeignKey("especie.idEspecie"), nullable=False)
+    nomeRaca = Column("NomeRaca", String(255), nullable=False)
+    especie_id = Column("Especie", Integer, ForeignKey("tabela_especie.idEspecie"), nullable=False)
+
+    relacaoEspecie = relationship("Especie", back_populates="relacaoRaca")
+    relacaoPaciente_4 = relationship("Paciente", back_populates="relacaoRaca_2")
 
 class Paciente(Base):
-    __tablename__ = "paciente"
+    __tablename__ = "tabela_paciente"
 
     id = Column("idPaciente", Integer, primary_key=True, autoincrement=True)
-    NomePaciente = Column("NomePaciente", String(255), nullable=False)
-    Peso = Column("Peso", Float)
-    Sexo = Column(Enum("M","F", name="enum_Sexo"), nullable=False)
-    DataDeNascimento = Column("DataDeNascimento", Date)
-    Raca = Column("Raca", Integer, ForeignKey("raca.idRaca"))
-    Tutor = Column("Tutor", Integer, ForeignKey("tutor.idTutor"))
+    nomePaciente = Column("NomePaciente", String(255), nullable=False)
+    peso = Column("Peso", Float)
+    sexo = Column(Enum("M","F", name="enum_Sexo"), nullable=False)
+    dataDeNascimento = Column("DataDeNascimento", Date)
+    raca_id = Column("Raca", Integer, ForeignKey("tabela_raca.idRaca"))
+    tutor_id = Column("Tutor", Integer, ForeignKey("tabela_tutor.idTutor"))
+
+    relacaoTutor = relationship("Tutor", back_populates="relacaoPaciente")
+    relacaoConsulta_2 = relationship("Consulta", back_populates="relacaoPaciente_2")
+    relacaoRegistroDeCondicaoDoPaciente = relationship("RegistroDeCondicaoDoPaciente", back_populates="relacaoPaciente_3")
+    relacaoRaca_2 = relationship("Raca", back_populates="relacaoPaciente_4")
 
 class RegistroDeCondicoes(Base):
-    __tablename__ = "registroDeCondicoes"
+    __tablename__ = "tabela_de_registros_de_condicoes"
 
     id = Column("idCondicao", Integer, primary_key=True, autoincrement=True)
-    NomeCondicao = Column("NomeCondicao", String(255), nullable=False)
-    Descricao = Column("Descricao", Text)
+    nomeCondicao = Column("NomeCondicao", String(255), nullable=False)
+    descricao = Column("Descricao", Text)
+
+    relacaoRegistroDeCondicaoDoPaciente_2 = relationship("RegistroDeCondicaoDoPaciente", back_populates="relacaoCondicao")
 
 class Consulta(Base):
-    __tablename__ = "consulta"
+    __tablename__ = "tabela_consultas"
 
     id = Column("idConsulta", Integer, primary_key=True, autoincrement=True)
-    Paciente = Column("idPaciente", ForeignKey("paciente.idPaciente"))
-    Veterinario = Column("idVeterinario", ForeignKey("veterinario.idVeterinario"))
-    Data = Column("Data", Date, nullable=False)
-    Observacoes = Column("Observacoes", Text)
+    paciente_id = Column("idPaciente", ForeignKey("tabela_paciente.idPaciente"))
+    veterinario_id = Column("idVeterinario", ForeignKey("tabela_veterinario.idVeterinario"))
+    data = Column("Data", Date, nullable=False)
+    observacoes = Column("Observacoes", Text)
+
+    relacaoVeterinario = relationship("Veterinario", back_populates="relacaoConsulta")
+    relacaoPaciente_2 = relationship("Paciente", back_populates="relacaoConsulta_2")
 
 class RegistroDeCondicaoDoPaciente(Base):
-    __tablename__ = "registroDeCondicaoDoPaciente"
+    __tablename__ = "tabela_de_registro_de_condicao_do_paciente"
 
     id = Column("idRegistro", Integer, primary_key=True, autoincrement=True)
-    Paciente = Column("idPaciente", Integer, ForeignKey("paciente.idPaciente"))
-    Condicao = Column("idCondicao", Integer, ForeignKey("registroDeCondicoes.idCondicao"))
-    Observacoes = Column("Observacoes", Text)
+    paciente_id = Column("idPaciente", Integer, ForeignKey("tabela_paciente.idPaciente"))
+    condicao_id = Column("idCondicao", Integer, ForeignKey("tabela_de_registros_de_condicoes.idCondicao"))
+    observacoes = Column("Observacoes", Text)
+
+    relacaoPaciente_3 = relationship("Paciente", back_populates="relacaoRegistroDeCondicaoDoPaciente")
+    relacaoCondicao = relationship("RegistroDeCondicoes", back_populates="relacaoRegistroDeCondicaoDoPaciente_2")
