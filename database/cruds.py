a2ce8153a3ff3b755ae69024b915bd643f13cae2
Nicolas Barbosa <ngranato11@gmail.com>
2025-11-09 21:58:12 -0300

    criei o create e o read da crud do Tutor

--- database/cruds.py
+++ database/cruds.py
@@ -7,4 +7,28 @@ class TutorCRUD:
     def __init__(self,db: Session):
         self.db = db
 
+    def criar_tutor(self, nomeTutor, endereco, cidade, telefone):
+        objetoNovoTutor = models.Tutor(
+            nomeTutor=nomeTutor,
+            endereco=endereco,
+            cidade=cidade,
+            telefone=telefone
+        )
+
+        self.db.add(objetoNovoTutor)
+        self.db.commit()
+        self.db.refresh(objetoNovoTutor)
+
+        return objetoNovoTutor
     
+    def lista_de_tutores_cadastrados(self):
+        return self.db.query(models.Tutor).all()
+    
+    def busca_por_tutor_pelo_nome(self, nome_tutor: str):
+        return self.db.query(models.Tutor).filter(models.Tutor.nomeTutor.like(f"%{nome_tutor}%")).all()
+    
+    def busca_por_tutor_pelo_ID(self, tutor_id: int):
+        return self.db.query(models.Tutor).filter(models.Tutor.id == tutor_id).first()
+
+    def atualizar_tutor(self, nome_novo: str):
+        return
